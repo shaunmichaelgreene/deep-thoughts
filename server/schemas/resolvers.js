5806dba97921aa37c8caef8d7d1f89d2d38ed42c
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-16 21:45:00 -0400

    end 21.2.4 JWT mutation added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,31 +1,51 @@
 const { User, Thought } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    Query: {
-        //get all thoughts
-        thoughts: async (parent, { username }) => {
-            const params = username ? { username } : {}
-            return Thought.find(params).sort({ createdAt: -1 });
-        },
-        //get a thought by ID
-        thought: async (parent, { _id }) => {
-            return Thought.findOne({ _id });
-        },
-        //get all users
-        users: async () => {
-            return User.find()
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        },
-        //get a user by username
-        user: async (parent, { username }) => {
-            return User.findOne({ username })
-                .select('-__v -password')
-                .populate('friends')
-                .populate('thoughts');
-        }
+  Query: {
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('friends')
+        .populate('thoughts');
+    },
+    thoughts: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Thought.find(params).sort({ createdAt: -1 });
+    },
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
     }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+        const user = await User.create(args);
+        const token = signToken(user);
+        return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new AuthenticationError('Incorrect credentials!');
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+        }
+        const token = signToken(user);
+        return { token, user };
+    }
+  }
 };
 
 module.exports = resolvers;
