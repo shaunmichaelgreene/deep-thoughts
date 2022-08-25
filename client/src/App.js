5e7c9f5a311164344454f5b2b89345a67f8137ff
Shaun Greene <shaunmichaelgreene@gmail.com>
2022-08-25 12:04:07 -0400

    end 21.4.6 FriendList, ReactionList components added, Profile page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import {
   ApolloClient,
   InMemoryCache,
@@ -17,7 +18,7 @@ import SingleThought from './pages/SingleThought';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import { Sign } from 'crypto';
-import { Thought } from '../../server/models';
+// import { Thought } from '../../server/models';
 
 const httpLink = createHttpLink({
   uri: '/graphql',
